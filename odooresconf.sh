bbf405dfb25deb5d5d64db0f514511652d224771
Luis Muro <luis.muro@xmarts.com>
2017-09-21 00:48:19 -0500

    se agrega copia sin dejar respaldo

--- odooresconf.sh
+++ odooresconf.sh
@@ -3,11 +3,12 @@
 export DIRECTORY=/home/respaldos/files/$CONTEXTO
 export CARPETA=`date +%d%m%y%H%M`
 export DIA=`date +%u`
-export FILES=/var/lib/odoo/.local/share/Odoo/filestore
+export FILES=/opt/odoo/.local/share/Odoo/filestore
 export DBUSER=dhu
 export DBHOST=127.0.0.1
 export DBPORT=5432
 export S3=y
 export SPACENAME=xmarts
 export SPACEPATH=unicornio
+export COMRATIO=3
 
--- respaldoDiario.sh
+++ respaldoDiario.sh
@@ -6,23 +6,39 @@ export CONTEXTO=$1
 
 . /usr/local/bin/odooresconf.sh
 
-
-echo "Copia de respaldo para "$CONTEXTO" con el nombre "$CONTEXTO$CARPETA" en "$DIRECTORY/diario
-mkdir -p $DIRECTORY/diario
-
-
-echo "Respaldo db $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz ...\n"
-
-pg_dump -U $DBUSER -h $DBHOST -p $DBPORT  $CONTEXTO | gzip -c > $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz
-
-
-echo "Respaldo files  $DIRECTORY/diario/$CONTEXTO-FILES-diario-$DIA.sql.gz ...\n"
-
-tar -czf $DIRECTORY/diario/$CONTEXTO-FILESdiario-$DIA.tar.gz  $FILES/$CONTEXTO
-
-if [ "$S3" = "y" ]; then
-	echo "Respaldo S3"
-	s3cmd put $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz  s3://$SPACENAME/$SPACEPATH/
-	s3cmd put $DIRECTORY/diario/$CONTEXTO-FILESdiario-$DIA.tar.gz  s3://$SPACENAME/$SPACEPATH/
+if [ "$2" = "local" ]; then
+	export S3="n"
 fi
 
+
+
+if [ "$S3" = "y" ]; then
+	echo "Respaldo SQL S3  s3://$SPACENAME/$SPACEPATH/ $CONTEXTO-DB-diario-$DIA.sql.xz "
+	
+#	pg_dump -U $DBUSER -h $DBHOST -p $DBPORT  $CONTEXTO | xz -c -$COMRATIO  - | s3cmd put - s3://$SPACENAME/$SPACEPATH/$CONTEXTO-DB-diario-$DIA.sql.xz
+	pg_dump -U $DBUSER -h $DBHOST -p $DBPORT  $CONTEXTO | gzip -c -$COMRATIO   | s3cmd put - s3://$SPACENAME/$SPACEPATH/$CONTEXTO-DB-diario-$DIA.sql.gz
+
+	
+        echo "Respaldo FILES S3  s3://$SPACENAME/$SPACEPATH/ $CONTEXTO-FILESdiario-$DIA.tar.xz "
+
+#	tar -cf - $FILES/$CONTEXTO | xz -c -$COMRATIO  - | s3cmd put - s3://$SPACENAME/$SPACEPATH/$CONTEXTO-FILESdiario-$DIA.tar.xz
+	tar -cf - $FILES/$CONTEXTO | gzip -c -$COMRATIO   | s3cmd put - s3://$SPACENAME/$SPACEPATH/$CONTEXTO-FILESdiario-$DIA.tar.gz
+
+
+	#s3cmd put $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz  s3://$SPACENAME/$SPACEPATH/
+	#s3cmd put $DIRECTORY/diario/$CONTEXTO-FILESdiario-$DIA.tar.gz  s3://$SPACENAME/$SPACEPATH/
+else
+	echo "Copia de respaldo para "$CONTEXTO" con el nombre "$CONTEXTO$CARPETA" en "$DIRECTORY/diario
+	mkdir -p $DIRECTORY/diario
+
+
+	echo "Respaldo db $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz ...\n"
+
+	pg_dump -U $DBUSER -h $DBHOST -p $DBPORT  $CONTEXTO | gzip -c > $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz
+
+
+	echo "Respaldo files  $DIRECTORY/diario/$CONTEXTO-FILES-diario-$DIA.sql.gz ...\n"
+
+	tar -czf $DIRECTORY/diario/$CONTEXTO-FILESdiario-$DIA.tar.gz  $FILES/$CONTEXTO
+
+fi
