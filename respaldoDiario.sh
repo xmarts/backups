95068b3549c89fd6e11902a3e82844047b4b607b
Luis Muro <luis.muro@xmarts.com>
2017-09-20 23:22:44 -0500

    Agrego nombre del respaldo

--- respaldoDiario.sh
+++ respaldoDiario.sh
@@ -11,18 +11,18 @@ echo "Copia de respaldo para "$CONTEXTO" con el nombre "$CONTEXTO$CARPETA" en "$
 mkdir -p $DIRECTORY/diario
 
 
-echo "Respaldo db $DIRECTORY/diario/DB-diario-$DIA.sql.gz ...\n"
+echo "Respaldo db $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz ...\n"
 
-pg_dump -U $DBUSER -h $DBHOST -p $DBPORT  $CONTEXTO | gzip -c > $DIRECTORY/diario/DB-diario-$DIA.sql.gz
+pg_dump -U $DBUSER -h $DBHOST -p $DBPORT  $CONTEXTO | gzip -c > $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz
 
 
-echo "Respaldo files  $DIRECTORY/diario/FILES-diario-$DIA.sql.gz ...\n"
+echo "Respaldo files  $DIRECTORY/diario/$CONTEXTO-FILES-diario-$DIA.sql.gz ...\n"
 
-tar -czf $DIRECTORY/diario/FILESdiario-$DIA.tar.gz  $FILES/$CONTEXTO
+tar -czf $DIRECTORY/diario/$CONTEXTO-FILESdiario-$DIA.tar.gz  $FILES/$CONTEXTO
 
 if [ "$S3" = "y" ]; then
 	echo "Respaldo S3"
-	s3cmd put $DIRECTORY/diario/DB-diario-$DIA.sql.gz  s3://$SPACENAME/$SPACEPATH/
-	s3cmd put $DIRECTORY/diario/FILESdiario-$DIA.tar.gz  s3://$SPACENAME/$SPACEPATH/
+	s3cmd put $DIRECTORY/diario/$CONTEXTO-DB-diario-$DIA.sql.gz  s3://$SPACENAME/$SPACEPATH/
+	s3cmd put $DIRECTORY/diario/$CONTEXTO-FILESdiario-$DIA.tar.gz  s3://$SPACENAME/$SPACEPATH/
 fi
 
